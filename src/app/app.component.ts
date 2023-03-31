02deaafd5f9bdc511c49e0b8e63286bd587c35ec
Mai Pham <mai291294@gmail.com>
2023-03-31 12:10:16 -0400

    import and fetch data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CPHead, Courses } from './cpinterfaces';  
+import cpdata from '../assets/data/cp.json'; //cpdata is a variable, so no {}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'jsonIntro';
+
+  cphead: CPHead = cpdata.ProgramData;
+  courses: Courses[] = cpdata.courses;
+
 }
