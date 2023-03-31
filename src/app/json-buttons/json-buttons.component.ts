139cee588b31b5744665f4463baf3582a0a7ddb0
Mai Pham <mai291294@gmail.com>
2023-03-31 13:15:35 -0400

    buttom for term1 & function on click

--- src/app/json-buttons/json-buttons.component.ts
+++ src/app/json-buttons/json-buttons.component.ts
@@ -9,4 +9,31 @@ import { CPHead, Courses } from '../cpinterfaces';
 export class JsonButtonsComponent {
   @Input() courses!: Courses[];
   @Input() cphead!: CPHead;
+
+  outarea!: string;
+
+  displayTerm(term: number) {
+    this.outarea = 
+    `
+      <h4>Term ${term} Classes: </h4>
+      <table>
+        <tr>
+          <th>Class</th>
+          <th>Description</th>
+        </tr>
+
+    `;
+
+    for (let x of this.courses) {
+      if (x.term === term) {
+        this.outarea += 
+        `
+          <tr>
+            <td>${x.class}</td>
+            <td>${x.description}</td>
+          </tr>
+        `;
+      }
+    }
+  }
 }
